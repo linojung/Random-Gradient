3cb4466897d2532300c581e1e15269ba96147c40
linojung <46038961+linojung@users.noreply.github.com>
2023-09-14 11:38:36 +0900

    more random

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
-const colors = [
-  "#ef5777",
-  "#575fcf",
-  "#4bcffa",
-  "#34e7e4",
-  "#0be881",
-  "#f53b57",
-  "#3c40c6",
-  "#0fbcf9",
-  "#00d8d6",
-  "#05c46b",
-  "#ffc048",
-  "#ffdd59",
-  "#ff5e57",
-  "#d2dae2",
-  "#485460",
-  "#ffa801",
-  "#ffd32a",
-  "#ff3f34",
-];
-
 function genGra() {
-  const count = colors.length;
-  let color1 = colors[Math.floor(Math.random() * count)];
-  let color2 = colors[Math.floor(Math.random() * count)];
-  let grad = `linear-gradient(to left, ${color1},${color2})`;
+  let color1 = {
+    r: Math.floor(Math.random() * 256),
+    g: Math.floor(Math.random() * 256),
+    b: Math.floor(Math.random() * 256),
+  };
+  let color2 = {
+    r: Math.floor(Math.random() * 256),
+    g: Math.floor(Math.random() * 256),
+    b: Math.floor(Math.random() * 256),
+  };
+
+  let from = `rgb(${color1.r},${color1.g},${color1.b})`;
+  let to = `rgb(${color2.r},${color2.g},${color2.b})`;
+
+  console.log(from, to);
+  let grad = `linear-gradient(to left, ${from},${to})`;
   document.body.style.background = grad;
 
   let info = document.querySelector(".info");
   let firstColor = info.querySelector("span:first-child");
   let SecondColor = info.querySelector("span:nth-child(2)");
-  firstColor.innerText = color1;
-  SecondColor.innerText = color2;
+  firstColor.innerText = from;
+  SecondColor.innerText = to;
 }
 
 const btn = document.querySelector("button");
